509bcc51a7b32cc7c6e6db1daf29d9fb3c3152fa
risemomentuman-commits <risemomentuman-commits@users.noreply.github.com>
2026-01-28 19:58:54 +0000

    deploy: 956a83422bb98e2b34df629988b61e57a4d93ada

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "490962892"
+    serviceWorkerVersion: "103696229"
   }
 });
