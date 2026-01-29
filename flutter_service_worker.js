1f6046f12e6120fa2076610f768319b8b73b12b1
risemomentuman-commits <risemomentuman-commits@users.noreply.github.com>
2026-01-29 15:18:59 +0000

    deploy: 64d5da747b846139a4bab79074c818f7a955ec29

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,9 +42,9 @@ const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "canvaskit/canvaskit.js.symbols": "efc2cd87d1ff6c586b7d4c7083063a40",
 "canvaskit/skwasm.js.symbols": "96263e00e3c9bd9cd878ead867c04f3c",
 "favicon.ico": "3b2fcaa7528d93581a575ad009186282",
-"flutter_bootstrap.js": "94822375280ab6309bbc6a90a79becc4",
+"flutter_bootstrap.js": "0fd228778482bda34c9580cbb950232a",
 "version.json": "fe528fe325ef445b3b74e5a0b8700405",
-"main.dart.js": "853c20287154e9acb46e5df57fda15ee"};
+"main.dart.js": "272ecaa3152789cd96ee2d56627d1bf2"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
