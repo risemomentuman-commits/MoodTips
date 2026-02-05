f9f60f03ebac88021842cb35fe22b0e48c7631ae
risemomentuman-commits <risemomentuman-commits@users.noreply.github.com>
2026-02-05 10:25:54 +0000

    deploy: 690c7f3552731df6500d79cba2dbb8791728b809

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "assets/assets/audio/nature_rain.mp3": "69782910dd0405fe4d6a48575fae7986",
 "assets/assets/audio/piano.mp3": "7fb468600886916cdae6060365dd437b",
 "assets/assets/audio/meditation.mp3": "b6e0c19d25962d1cb5592254040ea185",
-"assets/fonts/MaterialIcons-Regular.otf": "983608f6ec8ba5d46794080216ebebc5",
+"assets/fonts/MaterialIcons-Regular.otf": "566e31e51155c3bae45a52e198317691",
 "assets/NOTICES": "4faa0b7eea9d00bb5a7deb55b2cc0871",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
@@ -43,9 +43,9 @@ const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "canvaskit/canvaskit.js.symbols": "efc2cd87d1ff6c586b7d4c7083063a40",
 "canvaskit/skwasm.js.symbols": "96263e00e3c9bd9cd878ead867c04f3c",
 "favicon.ico": "3b2fcaa7528d93581a575ad009186282",
-"flutter_bootstrap.js": "447fd4a7383ba7751d4d698d1378599b",
+"flutter_bootstrap.js": "60109e571dcea31428baad55d6a031af",
 "version.json": "fe528fe325ef445b3b74e5a0b8700405",
-"main.dart.js": "9994d9a8b2408f843f156241e0db5c91"};
+"main.dart.js": "86635864ec0d4998fdeebd4cdb6ff6fc"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
