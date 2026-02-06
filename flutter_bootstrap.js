f7b34b1651e67981d7c89519d61cac74fc42ad18
risemomentuman-commits <risemomentuman-commits@users.noreply.github.com>
2026-02-06 10:07:34 +0000

    deploy: 83c129affb9888245c3efd5b7de9cff071a00fea

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "122139099"
+    serviceWorkerVersion: "2875241878"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -43,9 +43,9 @@ const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "canvaskit/canvaskit.js.symbols": "efc2cd87d1ff6c586b7d4c7083063a40",
 "canvaskit/skwasm.js.symbols": "96263e00e3c9bd9cd878ead867c04f3c",
 "favicon.ico": "3b2fcaa7528d93581a575ad009186282",
-"flutter_bootstrap.js": "4f8b2646631c393b11c6cd36c2851456",
+"flutter_bootstrap.js": "577b4c29852ac33498d14aa12036681c",
 "version.json": "fe528fe325ef445b3b74e5a0b8700405",
-"main.dart.js": "cac7f757db2e120ff62f5a52bacc96c3"};
+"main.dart.js": "462d38da5014d2460ef9b29e108c596f"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
